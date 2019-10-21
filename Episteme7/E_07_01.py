7c364c8d504c70ab5207f48d155a0c3b9c93edb2
John Panos <johncpanos@gmail.com>
2019-10-21 12:38:33 -0700

    Made changes to episteme

--- Episteme7/E_07_01.py
+++ Episteme7/E_07_01.py
@@ -1,7 +1,17 @@
+'''
+ @author John Panos, Marcus Kurschat (Westmont College)
+ @contact jpanos@westmont.edu, mkurschat@westmont.edu
+ Created on Mon Oct 21 2019 11:52:37 AM
+'''
+
 def wordCounter(filename, wordList):
+  # Init list for the count of words
   countList = [0] * len(wordList)
+  # 
   try:
+    # Get file handler for the filename
     myFile = open(filename, "r")
+    # For every line, split it into 
     for line in myFile:
       lineList = line.split(" ")
       for word in lineList:
@@ -9,20 +19,28 @@ def wordCounter(filename, wordList):
         for i in range(0, len(wordList)):
           if strippedWord == wordList[i].lower():
             countList[i] += 1
+  # Catch the error and let the user know that the file doesn't exist
   except IOError:
     print("Could not open the file:", filename)
+  # For every word output it to the console
   for i in range(0, len(wordList)):
       word = wordList[i].lower()
       wordCount = countList[i]
       print('{:^23}'.format(word + " : " + str(wordCount)))
 
+def wordFilter(filename):
+  try:
+    myFile = open(filename, "r")
+  except:
+    print("Could not open the file:", filename)
 
 filename = "FrozenScript.txt"
 print("*****")
 print(filename)
-wordCounter(filename, ["he", "she", "it", "Olaf", "Anna"])
+# wordCounter(filename, ["he", "she", "it", "Olaf", "Anna"])
+wordFilter(filename)
 print("*****")
 filename = "dummy.txt"
-print(filename)
-wordCounter(filename, ["he", "she", "it"])
-print("*****")
+# print(filename)
+# wordCounter(filename, ["he", "she", "it"])
+# print("*****")
